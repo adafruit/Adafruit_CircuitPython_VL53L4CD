45d388f453137d2e40f1f5af8d0b81d12c11a836
caternuson <caternuson@gmail.com>
2022-03-05 08:39:21 -0800

    black the cookie stuff

--- setup.py
+++ setup.py
@@ -56,7 +56,6 @@ setup(
     ],
     # What does your project relate to?
     keywords="adafruit blinka circuitpython micropython vl53l4cd distance",
-
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     # TODO: IF LIBRARY FILES ARE A PACKAGE FOLDER,
